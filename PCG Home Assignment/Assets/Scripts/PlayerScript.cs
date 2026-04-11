a32af6405daf50c560f2fd4557a70b5bb6913f91
Michael-Valletta <valletta.michael@gmail.com>
2026-04-11 13:55:59 +0200

    Finished Adding all The Tiles for the Map, Did the Knight(Player) Animations

--- PCG Home Assignment/Assets/Scripts/PlayerScript.cs
+++ PCG Home Assignment/Assets/Scripts/PlayerScript.cs
@@ -3,22 +3,32 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     public float moveSpeed = 8f;
-    public float attackRange = 1.2f; 
+    public float attackRange = 1.2f;
     public int attackDamage = 10;
-    public LayerMask enemyLayers;   
+    public LayerMask enemyLayers;
 
     private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sprite;
     private Vector2 moveInput;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
+
+        if (moveInput.x > 0) sprite.flipX = false;
+        else if (moveInput.x < 0) sprite.flipX = true;
+
+        float moveSpeedForAnim = moveInput.magnitude;
+        anim.SetFloat("Speed", moveSpeedForAnim);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -33,19 +43,19 @@ public class PlayerScript : MonoBehaviour
 
     void Attack()
     {
+        anim.SetTrigger("Attack");
+
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, attackRange, enemyLayers);
 
         foreach (Collider2D enemy in hitEnemies)
         {
             Debug.Log("We hit " + enemy.name);
-
             EnemyScript health = enemy.GetComponent<EnemyScript>();
             if (health != null)
             {
                 health.TakeDamage(attackDamage);
             }
         }
-        Debug.DrawRay(transform.position, Vector3.up * attackRange, Color.yellow, 0.2f);
     }
 
     void OnDrawGizmosSelected()
